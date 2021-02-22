82ee965714c614fa529139be3556e60acd6455c1
Jose Wesley <jwes.ofc1994@gmail.com>
2021-02-22 10:55:10 -0300

    desafio_2

---
+++ Ao_cubo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ao_cubo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int num1 = Int32.Parse(Console.ReadLine());
+           int num2 = Int32.Parse(Console.ReadLine());
+
+           for (int i = num1; i < num2 ; i++) {
+               double cubo = i*i*i;
+               
+               Console.WriteLine(cubo);
+
+           }
+           
+
+        }
+    }
+}
